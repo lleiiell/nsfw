1ad75331cfb4c1accfe338e11a9f91cc735732ed
tll <tangliangliang@lanjingren.com>
2019-01-23 00:29:53 +0800

    pre-load caffe model

--- nsfw.py
+++ nsfw.py
@@ -8,6 +8,18 @@ import caffe
 
 import classify_nsfw
 
+# Pre-load caffe model.
+nsfw_net = caffe.Net("nsfw_model/deploy.prototxt",  # pylint: disable=invalid-name
+                     "nsfw_model/resnet_50_1by2_nsfw.caffemodel", caffe.TEST)
+
+# Load transformer
+# Note that the parameters are hard-coded for best results
+caffe_transformer = caffe.io.Transformer({'data': nsfw_net.blobs['data'].data.shape})
+caffe_transformer.set_transpose('data', (2, 0, 1))  # move image channels to outermost
+caffe_transformer.set_mean('data', np.array([104, 117, 123]))  # subtract the dataset-mean value in each channel
+caffe_transformer.set_raw_scale('data', 255)  # rescale from [0, 1] to [0, 255]
+caffe_transformer.set_channel_swap('data', (2, 1, 0))  # swap channels from RGB to BGR
+
 app = Flask(__name__)
 
 @app.route('/ck')
@@ -21,18 +33,6 @@ def ck():
 
         handle = urlopen(u)
         image_data = handle.read()
-
-        # Pre-load caffe model.
-        nsfw_net = caffe.Net("nsfw_model/deploy.prototxt",  # pylint: disable=invalid-name
-            "nsfw_model/resnet_50_1by2_nsfw.caffemodel", caffe.TEST)
-
-        # Load transformer
-        # Note that the parameters are hard-coded for best results
-        caffe_transformer = caffe.io.Transformer({'data': nsfw_net.blobs['data'].data.shape})
-        caffe_transformer.set_transpose('data', (2, 0, 1))  # move image channels to outermost
-        caffe_transformer.set_mean('data', np.array([104, 117, 123]))  # subtract the dataset-mean value in each channel
-        caffe_transformer.set_raw_scale('data', 255)  # rescale from [0, 1] to [0, 255]
-        caffe_transformer.set_channel_swap('data', (2, 1, 0))  # swap channels from RGB to BGR
 
         # Classify.
         scores = classify_nsfw.caffe_preprocess_and_compute(image_data, caffe_transformer=caffe_transformer, caffe_net=nsfw_net, output_layers=['prob'])
